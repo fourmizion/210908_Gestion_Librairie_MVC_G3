2579f812110132885831431df520bef7db32a7e4
fourmizion <90311021+fourmizion@users.noreply.github.com>
2021-09-08 15:56:31 +0200

    Update header.php

--- Utils/header.php
+++ Utils/header.php
@@ -18,19 +18,19 @@
                 <div class="navElement">
                     <p>Livres</p>
                     <ul>
-                        <li><a href="?controller=livre&action=livre_result"> Tous les livres</a></li>
-                        <li><a href="?controller=livre&action=livre_auteur_form"> Trier par auteur</a></li>
-                        <li><a href="?controller=livre&action=livre_editeur_form"> Trier par éditeur</a></li>
-                        <li><a href="?controller=livre&action=livre_titre_form"> Trier par titre</a></li>
+                        <li><a href="?controller=livre&action=livre_result">Tous</a></li>
+                        <li><a href="?controller=livre&action=livre_auteur_form">Par auteur</a></li>
+                        <li><a href="?controller=livre&action=livre_editeur_form">Par éditeur</a></li>
+                        <li><a href="?controller=livre&action=livre_titre_form">Par titre</a></li>
                     </ul>
                 </div>
                 
                 <div class="navElement">
                     <p>Fournisseurs</p>
                     <ul>
-                        <li><a href="?controller=fournisseur&action=fournisseur_result">Tous les fournisseurs</a></li>
-                        <li><a href="?controller=fournisseur&action=fournisseur_raison_form">Trier par raison sociale</a></li>
-                        <li><a href="?controller=fournisseur&action=fournisseur_localite_form">Trier par localité</a></li>
+                        <li><a href="?controller=fournisseur&action=fournisseur_result">Tous</a></li>
+                        <li><a href="?controller=fournisseur&action=fournisseur_raison_form">Par raison sociale</a></li>
+                        <li><a href="?controller=fournisseur&action=fournisseur_localite_form">Par localité</a></li>
                     </ul>
                 </div>
                 
@@ -39,10 +39,10 @@
                 <div class="navElement">
                     <p>Commandes</p>
                     <ul>
-                        <li><a href="?controller=commande&action=commande_result">Toutes les commandes</a></li>
-                        <li><a href="?controller=commande&action=commande_livre_editeur_form">Trier par éditeur</a></li>
-                        <li><a href="?controller=commande&action=commande_fournisseur_raison_form">Trier par fournisseur</a></li>
-                        <li><a href="?controller=commande&action=commande_date_form">Trier par date</a></li>
+                        <li><a href="?controller=commande&action=commande_result">Toutes</a></li>
+                        <li><a href="?controller=commande&action=commande_livre_editeur_form">Par éditeur</a></li>
+                        <li><a href="?controller=commande&action=commande_fournisseur_raison_form">Par fournisseur</a></li>
+                        <li><a href="?controller=commande&action=commande_date_form">Par date</a></li>
                     </ul>
                 </div> 
                 <?php endif; endif; ?>  
